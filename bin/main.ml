b59cbd2a9d1500aa4a489994a5126aacb3614a56
Jamie Wales <jamie.wales.developer@gmail.com>
2024-08-17 11:27:25 +0100

    pwd

--- bin/main.ml
+++ bin/main.ml
@@ -1,11 +1,36 @@
-type builtin = Ls | Help
+type builtin = Ls | Cd of string | Help | Pwd
 type command = Exit | Echo of string | Builtin of builtin
+
+let home = ref (Unix.getenv "HOME" |> String.split_on_char '/')
+
+let addDir lst_ref str =
+  if str = "" then ()
+  else if str.[0] = '/' then
+    lst_ref :=
+      String.split_on_char '/' (String.sub str 1 (String.length str - 1))
+  else
+    (* Relative path: handle . and .. *)
+    let parts = String.split_on_char '/' str in
+    List.iter
+      (fun part ->
+         match part with
+         | "." -> ()
+         | ".." ->
+           if !lst_ref <> [] then
+             lst_ref := List.rev (List.tl (List.rev !lst_ref))
+         | _ -> lst_ref := !lst_ref @ [ part ])
+      parts
+
+let print_working_dir path = print_endline (String.concat "/" path)
 
 let parse_input input =
   match input with
   | "exit" -> Exit
   | "ls" -> Builtin Ls
   | "help" -> Builtin Help
+  | "pwd" -> Builtin Pwd
+  | s when String.length s >= 3 && String.sub s 0 3 = "cd " ->
+    Builtin (Cd (String.sub s 3 (String.length s - 3)))
   | _ -> Echo input
 
 let execute_command = function
@@ -18,7 +43,11 @@ let execute_command = function
   | Builtin b ->
     (match b with
      | Ls -> print_endline "Listing files... (not implemented)"
-     | Help -> print_endline "Available commands: exit, ls, help");
+     | Help ->
+       print_endline
+         "Available commands: exit, ls, help, pwd, cd <directory>"
+     | Cd dir -> addDir home dir
+     | Pwd -> print_working_dir !home);
     true
 
 let rec repl () =
@@ -33,6 +62,4 @@ let rec repl () =
     let continue = execute_command command in
     if continue then repl () else false
 
-let () =
-  print_endline "Welcome to the OCaml REPL. Type 'help' for available commands.";
-  ignore (repl ())
+let _ = repl ()
